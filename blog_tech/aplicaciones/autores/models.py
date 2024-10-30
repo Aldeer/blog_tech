087be02485735c91d4066173f4e9b6c136840ddf
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-10-30 17:29:49 -0300

    creado el modelo de autores basico

--- blog_tech/aplicaciones/autores/models.py
+++ blog_tech/aplicaciones/autores/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Autores(models.Model):
+    """
+    Clase modelo de autores
+    """
+
+    id_autor = models.AutoField(primary_key=True, verbose_name='ID Autor')
+    usuario = models.OneToOneField(User, verbose_name='Usuario', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        """
+        Retorna el nobmre de usuario del autor
+        """
+        return f'{self.usuario.username}'
