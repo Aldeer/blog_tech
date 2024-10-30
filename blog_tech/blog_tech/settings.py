103ef27ac3cfc3c5f32855e0ec65e384e9508cf7
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-10-30 17:26:01 -0300

    configuracion de las aplicaciones en el proyecto

--- blog_tech/blog_tech/settings.py
+++ blog_tech/blog_tech/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Aplicaciones
+    'aplicaciones.autores',
+    'aplicaciones.categorias',
+    'aplicaciones.comentarios',
+    'aplicaciones.publicaciones',
 ]
 
 MIDDLEWARE = [
