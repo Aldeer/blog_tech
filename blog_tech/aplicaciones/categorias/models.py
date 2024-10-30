790e412852329a496d5f956d7f7d595fb753bbff
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-10-30 17:35:58 -0300

    creado el modelo de categoria basico

--- blog_tech/aplicaciones/categorias/models.py
+++ blog_tech/aplicaciones/categorias/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    """
+    Clase model de Categoria
+    Campos:
+        - id_categoria:
+        - nombre:
+        - id_publicacion:
+    """
+
+    id_categoria = models.AutoField(primary_key=True, verbose_name='ID Categoria')
+    nombre = models.CharField(verbose_name='Nombre Categoria', max_length=255, blank=False, null=False)
+    
+
+
