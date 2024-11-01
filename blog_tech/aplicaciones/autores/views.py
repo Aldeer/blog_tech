2483f343424770a52855f2726366bec9c9eae5a9
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-11-01 17:40:35 -0300

    vista para la creacion de autores

--- blog_tech/aplicaciones/autores/views.py
+++ blog_tech/aplicaciones/autores/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import AutorForm, UserForm
 
 
 def home(request):
     return render(request, 'index.html')
+
+def registrar_autor(request):
+    
+    if request.method == 'POST':
+        pass
+    autor_form = AutorForm()
+    user_form = UserForm()
+    context = {'autor_form': autor_form, 'user_form': user_form}
+    return render(request, 'registro_autor.html', context)
