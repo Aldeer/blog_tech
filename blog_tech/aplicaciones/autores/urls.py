815370acb569915ce1db76e541a745190e69569d
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-11-04 17:10:48 -0300

    url de la vista login

--- blog_tech/aplicaciones/autores/urls.py
+++ blog_tech/aplicaciones/autores/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('registro_autor/', views.registrar_autor, name='registro_autor')
+    path('registro_autor/', views.registrar_autor, name='registro_autor'),
+    path('login/', views.login_autor, name='login')
 ]
